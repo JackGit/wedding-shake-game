b942f33e09f291e2ec1f472c9b4a92e845637640
Jack Yang <ygjack414@hotmail.com>
2016-03-08 18:04:31 +0800

    add test script

---
+++ build/wedding.jackyang.me test script.js
@@ -0,0 +1,112 @@
+var data = ["56de2e8ddf0eea00554412d1,测试用户325,BRIDE", "56de2e8d8ac2470056e60777,测试用户444,GROOM", "56de2e8dda2f600055ae458a,测试用户311,BRIDE", "56de2e8d5bbb500052339808,测试用户490,GROOM", "56de2e8da341310055ecb703,测试用户478,GROOM", "56de2e8d731956005e17d837,测试用户469,BRIDE", "56de2e8d8ac2470056e60776,测试用户296,GROOM", "56de2e8d128fe1005acc4abb,测试用户417,BRIDE", "56de2e8d5bbb500052339805,测试用户424,GROOM", "56de2e8d731956005c44b46b,测试用户309,BRIDE", "56de2e8defa631005ebfd096,测试用户335,BRIDE", "56de2e8defa631005ebfd095,测试用户360,GROOM", "56de2e8dc4c97100559cbb46,测试用户242,GROOM", "56de2e8dda2f600055ae4586,测试用户421,BRIDE", "56de2e8d8ac2470056e60775,测试用户212,GROOM", "56de2e8dc4c97100559cbb44,测试用户455,BRIDE", "56de2e8dc4c97100559cbb45,测试用户434,GROOM", "56de2e8d1ea493005edb9d2f,测试用户346,GROOM", "56de2e8dd342d3005550fdca,测试用户461,BRIDE", "56de2e8defa631005ebfd093,测试用户329,BRIDE", "56de2e8d731956005c44b46a,测试用户479,BRIDE", "56de2e8dda2f600055ae4582,测试用户352,GROOM", "56de2e8d7db2a20059427d6e,测试用户259,BRIDE", "56de2e8d1ea493005edb9d2e,测试用户148,GROOM", "56de2e8d1532bc0056205084,测试用户357,BRIDE", "56de2e8dc4c97100559cbb43,测试用户375,BRIDE", "56de2e8d731956005c44b469,测试用户383,BRIDE", "56de2e8dd342d3005550fdc9,测试用户319,BRIDE", "56de2e8d1532bc0056205083,测试用户291,BRIDE", "56de2e8d816dfa0052e6bcbf,测试用户215,BRIDE", "56de2e8d7db2a20059427d6d,测试用户376,GROOM", "56de2e8d731956005e17d836,测试用户396,GROOM", "56de2e8d731956005e17d835,测试用户433,BRIDE", "56de2e8d5bbb500052339802,测试用户366,GROOM", "56de2e8d1ea493005edb9d2d,测试用户422,GROOM", "56de2e8d731956005c44b468,测试用户494,GROOM", "56de2e8c5bbb500052339800,测试用户437,BRIDE", "56de2e8cc4c97100559cbb42,测试用户466,GROOM", "56de2e8c816dfa0052e6bcbe,测试用户355,BRIDE", "56de2e8c7664bf00559b2ad8,测试用户351,BRIDE", "56de2e8c7664bf00559b2ad7,测试用户390,GROOM", "56de2e8c128fe1005acc4ab7,测试用户429,BRIDE", "56de2e8c1ea493005edb9d2c,测试用户419,BRIDE", "56de2e8c731956005c44b467,测试用户398,GROOM", "56de2e8c731956005c44b466,测试用户439,BRIDE", "56de2e8cdf0eea00554412cb,测试用户397,BRIDE", "56de2e8cdf0eea00554412ca,测试用户413,BRIDE", "56de2e8c731956005c44b464,测试用户267,BRIDE", "56de2e8c1ea493005edb9d2b,测试用户425,BRIDE", "56de2e8c8ac2470056e60772,测试用户481,BRIDE", "56de2e8cefa631005ebfd091,测试用户488,GROOM", "56de2e8c731956005c44b463,测试用户465,BRIDE", "56de2e8cefa631005ebfd090,测试用户427,BRIDE", "56de2e8c1ea493005edb9d2a,测试用户389,BRIDE", "56de2e8cd342d3005550fdc7,测试用户411,BRIDE", "56de2e8c816dfa0052e6bcbc,测试用户486,GROOM", "56de2e8c8ac2470056e60771,测试用户219,BRIDE", "56de2e8c816dfa00590865de,测试用户354,GROOM", "56de2e8c816dfa00590865dd,测试用户282,GROOM", "56de2e8c7db2a20059427d6c,测试用户142,GROOM", "56de2e8c5bbb5000523397f6,测试用户430,GROOM", "56de2e8c1532bc0056205082,测试用户476,GROOM", "56de2e8ca341310055ecb701,测试用户484,GROOM", "56de2e8c128fe1005acc4ab5,测试用户196,GROOM", "56de2e8c816dfa00590865dc,测试用户193,BRIDE", "56de2e8cda2f600055ae457d,测试用户213,BRIDE", "56de2e8ca341310055ecb700,测试用户205,BRIDE", "56de2e8cc4c97100559cbb3f,测试用户197,BRIDE", "56de2e8cefa631005ebfd08d,测试用户220,GROOM", "56de2e8cda2f600055ae457c,测试用户224,GROOM", "56de2e8befa631005ebfd07b,测试用户327,BRIDE", "56de2e8b816dfa00590865d0,测试用户382,GROOM", "56de2e8a816dfa0052e6bc9f,测试用户345,BRIDE", "56de2e8a1ea493005edb9d11,测试用户385,BRIDE", "56de2e8a7664bf00559b2abd,测试用户321,BRIDE", "56de2e8a7db2a20059427d52,测试用户350,GROOM", "56de2e8aefa631005ebfd074,测试用户315,BRIDE", "56de2e8a8ac2470056e60752,测试用户470,GROOM", "56de2e8ad342d3005550fda9,测试用户269,BRIDE", "56de2e8af3609a00543f7f4b,测试用户399,BRIDE", "56de2e8ada2f600055ae4554,测试用户371,BRIDE", "56de2e8adf0eea00554412ab,测试用户475,BRIDE", "56de2e8ad342d3005550fda8,测试用户452,GROOM", "56de2e8ada2f600055ae4553,测试用户464,GROOM", "56de2e8ada2f600055ae4552,测试用户497,BRIDE", "56de2e8a8ac2470056e60751,测试用户480,GROOM", "56de2e8af3609a00543f7f4a,测试用户473,BRIDE", "56de2e8a1ea493005edb9d0f,测试用户472,GROOM", "56de2e8a816dfa0052e6bc9e,测试用户394,GROOM", "56de2e8aa341310055ecb6e7,测试用户487,BRIDE", "56de2e8a7db2a20059427d50,测试用户428,GROOM", "56de2e8a816dfa00590865c8,测试用户404,GROOM", "56de2e8ada2f600055ae4550,测试用户409,BRIDE", "56de2e8a1532bc0056205056,测试用户353,BRIDE", "56de2e8a731956005e17d81e,测试用户380,GROOM", "56de2e8a5bbb5000523397ca,测试用户483,BRIDE", "56de2e8af3609a00543f7f47,测试用户489,BRIDE", "56de2e8aa341310055ecb6e5,测试用户443,BRIDE", "56de2e8a8ac2470056e6074f,测试用户449,BRIDE", "56de2e8a731956005e17d81d,测试用户447,BRIDE"];
+var roomId = "56c2d539d342d30058e28d9f";
+var max = 10;
+
+data = data.map(function(d) {
+	var a = d.split(',');
+	return {
+		userId: a[0],
+		userName: a[1],
+		userType: a[2],
+		shakeCount: 0,
+		so: new wy.base.SteadyOutput({
+			max: 1,
+			interval: 1000,
+			overflow: 'shift'
+		});
+	};
+});
+
+function random(min, max) {
+	return Math.floor(min+Math.random()*(max-min));
+}
+
+
+function join(userId, userType, roomId) {
+	$.ajax({
+		type: 'POST',
+		url: '/game/user/joinRoom',
+		contentType: 'application/json',
+		dataType: 'json',
+		data: JSON.stringify({
+			userId: userId,
+			userType: userType,
+			roomId: roomId
+		}),
+		success: function(r) {
+			if(r.statusCode !== 0) {
+			console.error('error', r);
+			} else {
+				socket.emit('join', {userId: userId, roomId: roomId, userType: userType});
+			}
+		}
+	});
+}
+
+function startJoin(roomId) {
+	data.forEach(function(d, index) {
+		if(max && index > max)
+			return;
+
+		setTimeout(function() {
+			join(d.userId, d.userType, roomId);
+		}, random(0, 5000));
+	});
+}
+
+function startShake() {
+	data.forEach(function(d, index) {
+		if(max && index > max)
+			return;
+
+		so.register(function(user) {
+			updateUser(user);
+		});
+
+		d.so.start();
+		setTimeout(function() {
+			shake(d);
+		}, random(0, 500));
+	});
+
+	
+}
+
+function shake(user) {
+	var total = random(60, 150);
+	console.log(user.userName + ' started');
+
+	var seed = setInterval(function() {
+		if(user.shakeCount >= total)
+			clearInterval(seed);
+		else{
+			user.shakeCount ++;
+			console.log(user.userName + ' shake ' + user.shakeCount);
+			so.push({
+				objectId: user.userId,
+				shakeCount: user.shakeCount
+			});
+		}
+	}, random(100, 200));
+}
+
+
+
+
+function updateUser(user) {
+	user.objectId = user.userId;
+	$.ajax({
+		type: 'POST',
+		url: '/game/user/update',
+		contentType: 'application/json',
+		dataType: 'json',
+		data: JSON.stringify({user: user}),
+		success: function(r) {
+			if(r.statusCode !== 0) {
+				console.error('udpateUser error', r);
+			} else {
+				socket.emit('shake', {userId: user.userId, shakeCount: user.shakeCount});
+			}
+		}
+	});
+}
